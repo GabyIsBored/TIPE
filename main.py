031ccd442faf6b0dcd0018c7822c2834681972df
GabyIsBored <79170829+GabyIsBored@users.noreply.github.com>
2026-03-23 14:36:31 +0000

    unfinished

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from pprint import pprint
 import random 
 
+B=0
+J=1
+V=2
 
 def creer_dict():
     mots_de_longueurs = dict()
@@ -11,7 +14,6 @@ def creer_dict():
         for mot in les_mots:
             mots_de_longueurs[len(mot)].append(mot[:-1])
     return mots_de_longueurs
-
 
 def wordle_essai(solution, essai):
     occurences = {}
@@ -24,14 +26,13 @@ def wordle_essai(solution, essai):
         lettre = essai[i]
         if occurences.get(lettre, 0) > 0:
             if solution[i] == lettre:
-                res.append("vert")
+                res.append(V)
             else:
-                 res.append("jaune")
+                 res.append(J)
             occurences[lettre] -= 1
         else:
-             res.append("blanc")
+             res.append(B)
     return res
-
 
 def wordle_game(taille_mot, nb_essais):
     if taille_mot < 2: 
@@ -42,12 +43,12 @@ def wordle_game(taille_mot, nb_essais):
 
     solution = random.choice(mots_valables)
 
-    etat_gagne =  ["vert"] * taille_mot
+    etat_gagne =  [V] * taille_mot
     while nb_essais > 0:
         
-        # FOR TESTING - Naive checking function
+        # Amelioration possible: Table de hachage
         essai = input('Faire un essai: ').upper()
-        if essai not in mots_de_longueurs: 
+        if essai not in mots_valables: 
             print('Pas un mot valable!')
             nb_essais += 1
         # -------------------------------
@@ -59,5 +60,35 @@ def wordle_game(taille_mot, nb_essais):
         nb_essais -= 1
     return f'Perdu... Le mot etait {solution}'
 
-
+# -----------------------------------------------------------------------------------------------------------------------------------------
 print(wordle_game(5, 6))
+
+def generer_couleurs(): # liste des combinaisons possibles
+    return
+
+def mots_selon_couleur(essai): # liste des mots possibles selon les couleurs donnes par un essai 
+    # Partie redondante si mots_valables passe en argument 
+    taille_mot = len(essai)
+
+    if taille_mot < 2: 
+        return "Parametre invalide: Taille mot trop petite!"
+    
+    mots_de_longueurs = creer_dict()
+    mots_valables = mots_de_longueurs[taille_mot + 1]
+    # -----------------------------------------------------
+
+    mots_possibles = mots_valables
+    couleurs = [B, B, B, B, B]
+    mots_contraintes = []
+
+    for mot in mots_possibles:
+        couleurs_test = wordle_essai(mot,essai)
+        for i in range(taille_mot):
+            if couleur_lettre[] == couleurs_test[]:
+                
+
+
+    return
+
+def calcul_entropie():  
+    return
